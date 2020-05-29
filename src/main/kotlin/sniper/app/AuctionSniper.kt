5f5e9f3e28079fcb2ceef207dd3a5dd9046a6a1a
Ravil Galeyev <dehasi@yandex.ru>
2020-05-29 19:00:46 +0200

    [RED] Make the test work

--- src/main/kotlin/sniper/app/AuctionSniper.kt
+++ src/main/kotlin/sniper/app/AuctionSniper.kt
@@ -1,6 +1,12 @@
 package sniper.app
 
-class AuctionSniper {
-    fun auctionClosed() {
+class AuctionSniper(private val sniperListener: SniperListener) : AuctionEventListener {
+
+    override fun auctionClosed() {
+        sniperListener.sniperLost()
+    }
+
+    override fun currentPrice(price: Int, increment: Int) {
+        TODO("Not yet implemented")
     }
 }
