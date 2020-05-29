dabcb26bc2ed2b262857bdefc762a089ca8ad84d
Ravil Galeyev <dehasi@yandex.ru>
2020-05-29 18:50:42 +0200

    Refactoring

--- src/main/kotlin/sniper/app/AuctionMessageTranslator.kt
+++ src/main/kotlin/sniper/app/AuctionMessageTranslator.kt
@@ -7,12 +7,12 @@ import org.jivesoftware.smack.packet.Message
 open class AuctionMessageTranslator(private val listener: AuctionEventListener) : MessageListener {
 
     override fun processMessage(chat: Chat?, message: Message?) {
-
         val event: Map<String, String> = unpackedEventFrom(message!!)
+
         val type = event["Event"]
         if (type == "CLOSE") listener.auctionClosed()
-        else if (type == "PRICE") listener.currentPrice(event["CurrentPrice"]!!.toInt(),
-                event["Increment"]!!.toInt())
+        else if (type == "PRICE")
+            listener.currentPrice(event["CurrentPrice"]!!.toInt(), event["Increment"]!!.toInt())
     }
 
     private fun unpackedEventFrom(message: Message): Map<String, String> {
