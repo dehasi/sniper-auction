3633285c0c16ba3e7ec3d6b1c146ac08eca7c123
Ravil Galeyev <dehasi@yandex.ru>
2020-05-31 22:42:39 +0200

    [GREEN] Extract AuctionEvent class

--- src/main/kotlin/sniper/app/AuctionMessageTranslator.kt
+++ src/main/kotlin/sniper/app/AuctionMessageTranslator.kt
@@ -7,13 +7,14 @@ import org.jivesoftware.smack.packet.Message
 open class AuctionMessageTranslator(private val listener: AuctionEventListener) : MessageListener {
 
     override fun processMessage(chat: Chat?, message: Message?) {
-        val event: Map<String, String> = unpackedEventFrom(message!!)
+        val legacyEvent: Map<String, String> = unpackedEventFrom(message!!)
+        val event = AuctionEvent.from(message.body)
 
-        val type = event["Event"]
+        val type = event.type() //event["Event"]
 
         when (type) {
             "CLOSE" -> listener.auctionClosed()
-            "PRICE" -> listener.currentPrice(event["CurrentPrice"]!!.toInt(), event["Increment"]!!.toInt())
+            "PRICE" -> listener.currentPrice(event.currentPrice(), event.increment())
             else -> println("Unknown event:$event")
         }
     }
@@ -23,5 +24,47 @@ open class AuctionMessageTranslator(private val listener: AuctionEventListener) 
                 .map { it.trim() }
                 .map { it.split(":")[0].trim() to it.split(":")[1].trim() }
                 .toMap()
+    }
+
+    private class AuctionEvent {
+        private var fields = mutableMapOf<String, String>()
+        fun type(): String {
+            return get("Event")
+        }
+
+        fun currentPrice(): Int {
+            return getInt("CurrentPrice")
+        }
+
+        fun increment(): Int {
+            return getInt("Increment")
+        }
+
+        private fun getInt(fieldName: String): Int {
+            return get(fieldName).toInt()
+        }
+
+        private fun get(fieldName: String): String {
+            return fields[fieldName]!!
+        }
+
+        private fun addField(field: String) {
+            val pair = field.split(":")
+            fields[pair[0].trim()] = pair[1].trim()
+        }
+
+        companion object {
+            fun from(messageBody: String): AuctionEvent {
+                val event = AuctionEvent()
+                for (field in fieldsIn(messageBody)) {
+                    event.addField(field)
+                }
+                return event
+            }
+
+            fun fieldsIn(messageBody: String): List<String> {
+                return messageBody.split(";")
+            }
+        }
     }
 }
