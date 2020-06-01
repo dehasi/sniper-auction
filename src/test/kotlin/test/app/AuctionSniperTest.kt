6c1f85d3097fb3c4c0a6753c3d8acbfa8ec1eb9e
Ravil Galeyev <dehasi@yandex.ru>
2020-06-01 23:15:53 +0200

    Add some magic to mock state

--- src/test/kotlin/test/app/AuctionSniperTest.kt
+++ src/test/kotlin/test/app/AuctionSniperTest.kt
@@ -1,9 +1,11 @@
 package test.app
 
+import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.spy
 import org.mockito.Mockito.verify
 import org.mockito.junit.jupiter.MockitoExtension
 import sniper.app.Auction
@@ -11,18 +13,36 @@ import sniper.app.AuctionEventListener.PriceSource.FromOtherBidder
 import sniper.app.AuctionEventListener.PriceSource.FromSniper
 import sniper.app.AuctionSniper
 import sniper.app.SniperListener
+import test.app.AuctionSniperTest.SniperState.bidding
+import test.app.AuctionSniperTest.SniperState.idle
 
 @ExtendWith(MockitoExtension::class)
 class AuctionSniperTest {
 
-    @Mock private lateinit var sniperListener: SniperListener
     @Mock private lateinit var auction: Auction
-    @InjectMocks private lateinit var sniper: AuctionSniper
 
-    @Test fun returnsLostWhenAuctionCloses() {
+    private var sniperState = idle;
+    private var sniperListener: SniperListener = spy(SniperListenerStub())
+
+    private lateinit var sniper: AuctionSniper
+
+    @BeforeEach fun createSniper() {
+        sniper = AuctionSniper(auction, sniperListener)
+    }
+
+    @Test fun returnsLostWhenAuctionClosesImmediately() {
         sniper.auctionClosed()
 
         verify(sniperListener).sniperLost()
+    }
+
+
+    @Test fun returnsLostWhenAuctionClosesWhenBidding() {
+        sniper.currentPrice(123, 45, FromOtherBidder)
+        sniper.auctionClosed()
+
+        verify(sniperListener).sniperLost()
+        assertThat(sniperState).isEqualTo(bidding)
     }
 
     @Test internal fun bidsHigherAndReportsBiddingWhenNewPriceArrives() {
@@ -38,5 +58,17 @@ class AuctionSniperTest {
         sniper.currentPrice(123, 45, FromSniper)
 
         verify(sniperListener).sniperWinning()
+    }
+
+    private enum class SniperState {
+        idle, winning, bidding
+    }
+
+    private open inner class SniperListenerStub : SniperListener {
+        override fun sniperLost() {}
+        override fun sniperWinning() {}
+        override fun sniperBidding() {
+            sniperState = bidding
+        }
     }
 }
