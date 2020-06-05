d7e63879160674d82d0f1872c235a36da49e9c65
Ravil Galeyev <dehasi@yandex.ru>
2020-06-05 22:00:30 +0200

    [GREEN] Refactoring

--- src/main/kotlin/sniper/view/SnipersTableModel.kt
+++ src/main/kotlin/sniper/view/SnipersTableModel.kt
@@ -4,6 +4,7 @@ import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections.observableArrayList
 import sniper.app.SniperListener.SniperSnapshot
+import sniper.app.SniperState
 import sniper.app.SniperState.JOINING
 import sniper.view.MainView.Companion.STATUS_BIDDING
 import sniper.view.MainView.Companion.STATUS_JOINING
@@ -38,7 +39,9 @@ class SniperStateData(snapshot: SniperSnapshot) {
     val itemId = SimpleStringProperty(snapshot.itemId)
     val lastPrice = SimpleIntegerProperty(snapshot.lastPrice)
     val lastBid = SimpleIntegerProperty(snapshot.lastBid)
-    val status = SimpleStringProperty(STATUS_TEXT[snapshot.state.ordinal])
+    val status = SimpleStringProperty(textFor(snapshot.state))
+
+    private fun textFor(state: SniperState) = STATUS_TEXT[state.ordinal]
 
     companion object {
         private val STATUS_TEXT = listOf(STATUS_JOINING, STATUS_BIDDING, STATUS_WINNING, STATUS_LOST, STATUS_WON)
