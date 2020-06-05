55e36e146e10d272826a82d47219c0dac3f171f3
Ravil Galeyev <dehasi@yandex.ru>
2020-06-05 22:03:23 +0200

    [GREEN] Refactoring

--- src/main/kotlin/sniper/app/SniperListener.kt
+++ src/main/kotlin/sniper/app/SniperListener.kt
@@ -8,10 +8,10 @@ interface SniperListener {
     fun sniperStateChanged(snapshot: SniperSnapshot)
 
     data class SniperSnapshot(val itemId: String, val lastPrice: Int, val lastBid: Int, val state: SniperState) {
+
         fun winning(price: Int) = SniperSnapshot(itemId, price, lastBid, WINNING)
         fun bidding(price: Int, bid: Int) = SniperSnapshot(itemId, price, bid, BIDDING)
         fun closed()= SniperSnapshot(itemId, lastPrice, lastBid, state.whenAuctionClosed())
-
 
         companion object {
             fun joining(itemId: String) = SniperSnapshot(itemId, 0, 0, SniperState.JOINING)
