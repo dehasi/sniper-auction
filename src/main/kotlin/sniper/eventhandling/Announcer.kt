0d992b2c74c9d20222f4a5156134cc57de909180
Ravil Galeyev <dehasi@yandex.ru>
2020-06-14 21:47:50 +0200

    Make asserion failing

--- src/main/kotlin/sniper/eventhandling/Announcer.kt
+++ src/main/kotlin/sniper/eventhandling/Announcer.kt
@@ -1,13 +1,24 @@
 package sniper.eventhandling
 
+import java.lang.reflect.Proxy
 import java.util.*
 
 class Announcer<LISTENER : EventListener>(type: Class<out LISTENER>) {
 
     private val listeners = mutableListOf<LISTENER>()
+    private val proxy: LISTENER
+
+    init {
+        proxy = type.cast(Proxy.newProxyInstance(
+                type.classLoader, arrayOf<Class<*>>(type)
+
+        ){any, method, arrayOfAnys ->null
+
+        })
+    }
 
     fun announce(): LISTENER {
-        TODO()
+        return proxy
     }
 
     fun addListener(listener: LISTENER) {
