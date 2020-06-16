95926f5bb999ccda9e34eb51b76e39f3243d0a9d
Ravil Galeyev <dehasi@yandex.ru>
2020-06-16 23:02:48 +0200

    Add test

--- src/test/kotlin/sniper/app/SniperLauncherTest.kt
+++ src/test/kotlin/sniper/app/SniperLauncherTest.kt
@@ -1,19 +1,42 @@
 package sniper.app
 
+import io.mockk.every
 import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import sniper.view.SnipersTableModel
 
 
 internal class SniperLauncherTest {
 
     private val auctionHouse: AuctionHouse = mockk()
-    private val snipers: SnipersTableModel= mockk()
-    private val launcher = SniperLauncher(auctionHouse, snipers)
+    private val sniperCollector: SniperCollector = mockk()
+    private val launcher = SniperLauncher(auctionHouse, sniperCollector)
+
+    private val stateNotJoined = "not joined"
+    private val stateJoined = "joined"
 
     @Test fun `add a new sniper to collector and then join an auction`() {
         val itemId = "item 123"
+        var state = stateNotJoined
+        val auctionStub = object : Auction {
+            override fun join() {
+                state = stateJoined
+            }
 
+            override fun bid(amount: Int) {
+                TODO("Isn't called")
+            }
+
+            override fun addAuctionEventListener(auctionEventListener: AuctionEventListener) {
+                assertThat(state).isEqualTo(stateNotJoined)
+            }
+        }
+        every { auctionHouse.auctionFor(itemId) } returns auctionStub
+        every { sniperCollector.addSniper(any()) } answers {
+            assertThat(state).isEqualTo(stateNotJoined)
+        }
         launcher.joinAuction(itemId)
+
+        assertThat(state).isEqualTo(stateJoined)
     }
 }
